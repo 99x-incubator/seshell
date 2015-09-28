48d30dc412af7448d76fd05605b10cb296476dcc
zaurik <a.ruzaik@gmail.com>
2015-09-28 13:20:28 +0530

    Added "DeploymentScripts" folder and packages from NuGet

---
+++ DeploymentScripts/SeShellAutomationFramework - Copy.bat
@@ -0,0 +1,12 @@
+del /s C:\FinalTestResult\*.xml 
+cd ..
+cd SeShellTest
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe SeShell.Test.csproj /p:configuration=debug
+cd bin\debug
+nunit-console /fixture:SeShell.Test.TestCases.TestSuite SeShell.Test.dll
+cd ..
+cd ..
+cd ..
+cd SeShell.Test.XMLTestResult\bin\Debug\
+SeShell.Test.XMLTestResult.exe
+del /s C:\SeShellResult\*.xml 
